9299078f99b3c6d0f7946ac38ffaceed943dfca4
Atulrajthakre12 <306983340+Atulrajthakre12@users.noreply.github.com>
2026-07-25 21:45:47 +0530

    updated hr

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     alert("स्वागत है! अतुल राज ठाकरे की आधिकारिक वेबसाइट पर।");
 });
+function togglePoetry(header) {
+    const card = header.parentElement;
+    const body = card.querySelector('.poetry-body');
+    
+    if (body.style.display === "none") {
+        body.style.display = "block";
+        card.classList.add("active");
+    } else {
+        body.style.display = "none";
+        card.classList.remove("active");
+    }
+}
